7c0a8a33bc9d209d88c9ac1dddcc8a4e12221bbb
Chiara Rasi <rasi.chiara@gmacil.com>
2024-03-14 09:50:50 +0100

    Fix test

--- tests/test_validate.py
+++ tests/test_validate.py
@@ -1,13 +1,17 @@
 import json
 from preClinVar.validate import validate_submission
-from preClinVar.demo import subm_json_path
+from preClinVar.demo import clinvar_germline_hgvs_path
+
+
+def validate_submission_clinvar_examples():
+    """Tests validation of submission_examples from ClinVar against their schema."""
 
 
 def test_validate_submission():
     """Test the function that validates a json submission against the ClinVar API submission schema,"""
 
     # GIVEN a valid json file
-    assert json.load(open(subm_json_path))
-    with open(subm_json_path) as json_file:
+    assert json.load(open(clinvar_germline_hgvs_path))
+    with open(clinvar_germline_hgvs_path) as json_file:
         submission_dict = json.load(json_file)
-        assert validate_submission(submission_dict=submission_dict) == [True, []]
+        assert validate_submission(submission_dict=submission_dict) == (True, [])
