cf5777c07fd81a8e2db92bde11693d3c6f34613b
Chiara Rasi <rasi.chiara@gmacil.com>
2024-04-26 10:07:26 +0200

    Remove empty space

--- preClinVar/constants.py
+++ preClinVar/constants.py
@@ -1,7 +1,6 @@
 DRY_RUN_SUBMISSION_URL = "https://submit.ncbi.nlm.nih.gov/api/v1/submissions/?dry-run=true"
 VALIDATE_SUBMISSION_URL = "https://submit.ncbi.nlm.nih.gov/apitest/v1/submissions"
 
-
 CLNSIG_TERMS = [
     "Pathogenic",
     "Likely pathogenic",
